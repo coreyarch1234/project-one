6f7a55121d27663093cbe65ce08994f536e3108a
coreyarch1234 <harrilal@mit.edu>
2017-01-31 21:06:13 -0800

    worked on mongo

--- server.js
+++ server.js
@@ -10,6 +10,7 @@
 //
 // server.listen('3000');
 
+//Express Set
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
@@ -20,6 +21,13 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+
+// getting-started.js Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Post = require("../models/post.js");
+
 //Root route
 app.get('/', function (req, res) {
     res.render('home');
@@ -27,11 +35,17 @@ app.get('/', function (req, res) {
 
 //Posts route//INDEX
 app.get('/posts', function(req, res){
-    var posts = [
-        {body:'This is my first post.'},
-        {body:'This is my second post'},
-        {body:'This is my third post'}
-    ];
+    // var posts = [
+    //     {body:'This is my first post.'},
+    //     {body:'This is my second post'},
+    //     {body:'This is my third post'}
+    // ];
+
+    Post.findOne({ 'body': 'Hello World' }, 'name occupation', function (err, person) {
+      if (err) return handleError(err);
+      console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation) // Space Ghost is a talk show host.
+    })
+
     // res.json(posts);
     res.render('posts-index', {posts: posts});
 });
